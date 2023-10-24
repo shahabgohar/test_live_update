76587ef71f148fed4460750bf0c0f0aabfd4657f
shahabgohar <shahab.developer.work@gmail.com>
2023-10-24 17:46:48 +0500

    updated text for younus

--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="container">
     <strong>{{ name }}</strong>
-    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">Hello Shahab gohar!</a></p>
+    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">Hello Younus!</a></p>
   </div>
 </template>
 
